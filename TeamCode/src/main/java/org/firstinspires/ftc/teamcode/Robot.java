4dda1e5f57b4e59340fc47d2e908e1f0130c3cb6
DJ Shabsifhe <55489165+DJShabsifhe@users.noreply.github.com>
2024-10-16 23:57:50 +0800

    Basic structure and dependencies

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.subsystems.drivetrain.Drivetrain;
+
+public class Robot {
+
+    public Robot(HardwareMap hardwareMap) {
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.utils.Globals;
+import org.firstinspires.ftc.teamcode.utils.RunMode;
+
+public class TeleOp extends LinearOpMode {
+
+    /**
+     * Override this method and place your code here.
+     * <p>
+     * Please do not catch {@link InterruptedException}s that are thrown in your OpMode
+     * unless you are doing it to perform some brief cleanup, in which case you must exit
+     * immediately afterward. Once the OpMode has been told to stop, your ability to
+     * control hardware will be limited.
+     *
+     * @throws InterruptedException When the OpMode is stopped while calling a method
+     *                              that can throw {@link InterruptedException}
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Globals.RUNMODE = RunMode.TELEOP;
+
+//        Robot robot = new Robot(robotMap)
+
+    }
+}
